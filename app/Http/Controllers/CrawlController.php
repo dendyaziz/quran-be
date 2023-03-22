5b2220b91bc93f68678c60a92556e959738f37ed
Dendy Armandiaz Aziz <dendy.aziz@gmail.com>
2023-03-22 10:59:27 +0700

    first commit

---
+++ app/Http/Controllers/CrawlController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CrawlController extends Controller
+{
+    public function crawl() {
+        return ['dawdw'];
+    }
+}
